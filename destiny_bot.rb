b11b5031d7daaf37fcb22cde43869e0e0e8fdb38
Minkyu Kim <synthdnb@gmail.com>
2019-10-23 07:52:37 +0000

    Sort by rarity and index desc

--- destiny_bot.rb
+++ destiny_bot.rb
@@ -4,20 +4,24 @@ require 'oj'
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-bot = Discordrb::Bot.new token: ENV['TOKEN'], client_id: ENV['CLIENT_ID']
-
 en_data = Oj.load_file('./data/en.json');
 ko_data = Oj.load_file('./data/ko.json');
 data = en_data + ko_data
+
+
+bot = Discordrb::Bot.new token: ENV['TOKEN'], client_id: ENV['CLIENT_ID']
+
 
 bot.message(contains: /\[\[([^:]+)\]\]/) do |event|
   event.message.content =~ /\[\[([^:]+)\]\]/
   q = $1
   next if q.length < 3
   
-  if f = data.find{|x| x["name"] =~ /#{q}/i}
+  filtered = data.select{|x| x["name"] =~ /#{q}/i}.sort_by{|x| [-x["tierType"], -x["index"]]}
+
+  if f = filtered.first
     event.channel.send_embed do |embed|
-      embed.title = "#{f["name"]} [#{f["tiertype"]}]"
+      embed.title = "#{f["name"]} [#{f["typeAndTierName"]}]"
       embed.description = f["desc"]
       embed.url = "https://www.light.gg/db/ko/items/#{f["hash"]}"
       embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: "https://www.bungie.net/#{f["icon"]}")
